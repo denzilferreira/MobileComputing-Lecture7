bfb153196dcb4bb3b391bf83d0139953c382d0fb
Denzil Ferreira <ferreiradenzil@gmail.com>
2020-02-22 21:06:45 +0200

    Own camera: add OpenCV, apply real-time image analysis for black and white filter

--- app/src/main/java/com/example/mobilecomputinglecture7/OwnCamera.kt
+++ app/src/main/java/com/example/mobilecomputinglecture7/OwnCamera.kt
@@ -6,31 +6,37 @@ import android.os.Bundle
 import android.os.Environment
 import android.view.TextureView
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
-
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_own_camera.*
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
 import java.io.File
 import java.util.concurrent.Executors
 
 class OwnCamera : AppCompatActivity() {
 
     val PERMISSIONS_NEEDED_CODE = 12345
+    lateinit var overlay : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_own_camera)
         viewFinder = findViewById(R.id.camera_view)
+        overlay = findViewById(R.id.overlay_filter)
 
         if (!isCameraAllowed()) {
             ActivityCompat.requestPermissions(this,
                 arrayOf(
-                    android.Manifest.permission.CAMERA,
+                    Manifest.permission.CAMERA,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE
                 ),
                 PERMISSIONS_NEEDED_CODE
@@ -64,7 +70,7 @@ class OwnCamera : AppCompatActivity() {
             val file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"OwnCamera-${System.currentTimeMillis()}.jpg")
             imageCapture.takePicture(file, executor, object: ImageCapture.OnImageSavedListener {
                 override fun onImageSaved(file: File) {
-                    val message = "Photo saved: ${file.absolutePath}"
+                    val message = "Photo saved!"
                     Snackbar.make(own_camera, message, Snackbar.LENGTH_SHORT).show()
                 }
 
@@ -73,10 +79,36 @@ class OwnCamera : AppCompatActivity() {
                     message: String,
                     cause: Throwable?
                 ) {
-                    //oops
+                    Snackbar.make(own_camera, "Failed to save photo. Out of space?", Snackbar.LENGTH_SHORT).show()
                 }
 
             })
+        }
+
+
+        val imageAnalysisConfig = ImageAnalysisConfig.Builder().apply {
+            setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+            setImageQueueDepth(1)
+        }.build()
+
+        val imageAnalysis = ImageAnalysis(imageAnalysisConfig)
+
+        btn_filter.setOnClickListener {
+            if(OpenCVLoader.initDebug()) {
+                val black_n_white = Imgproc.COLOR_RGB2GRAY
+                imageAnalysis.setAnalyzer(executor, object : ImageAnalysis.Analyzer {
+                    override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
+                        val img = viewFinder.getBitmap()
+                        val mat = Mat()
+                        Utils.bitmapToMat(img, mat)
+                        Imgproc.cvtColor(mat, mat, black_n_white)
+                        Utils.matToBitmap(mat, img)
+                        runOnUiThread {
+                            overlay.setImageBitmap(img)
+                        }
+                    }
+                })
+            }
         }
 
         btn_flash.setOnClickListener {
@@ -85,7 +117,7 @@ class OwnCamera : AppCompatActivity() {
             } else preview.enableTorch(true)
         }
 
-        CameraX.bindToLifecycle(this, preview, imageCapture)
+        CameraX.bindToLifecycle(this, preview, imageCapture, imageAnalysis)
     }
 
     fun isCameraAllowed(): Boolean = (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
